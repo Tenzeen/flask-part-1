7a2ad4bd2e95af26e3c4fc0fcfdb1dd059bd7d12
Tenzin <110137277+Tenzeen@users.noreply.github.com>
2022-09-06 22:05:00 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+#import package
+from flask import Flask
+
+#Creating flask application
+app = Flask(__name__)
+
+#Home page that users will see first
+@app.route('/')
+def home():
+    return 'Hello world!'
+#About page
+@app.route('/about')
+def about():
+    return 'About us!'
+
+#Contact us page
+@app.route('/contact_us')
+def contact_us():
+    return 'You can contact us through email: "random@gmail.com'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=80)
+
